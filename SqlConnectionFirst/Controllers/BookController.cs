cd8c7b0eb57bb7c9b3861323a1ea89c5400a1f3e
Afganli97 <ulviva@code.edu.az>
2023-01-10 01:45:57 +0400

    Create SqlConnecton project

---
+++ SqlConnectionFirst/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SqlConnectionFirst.DAL;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SqlConnectionFirst.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly DataBase _dataBase;
+
+        public BookController(DataBase dataBase)
+        {
+            _dataBase = dataBase;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_dataBase.Books.ToList());
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View(_dataBase.Books.ToList().Find(x=>x.Id==id));
+        }
+    }
+}
---
+++ SqlConnectionFirst/DAL/DataBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SqlConnectionFirst.Models;
+
+namespace SqlConnectionFirst.DAL
+{
+    public class DataBase:DbContext
+    {
+        public DataBase(DbContextOptions<DataBase> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
